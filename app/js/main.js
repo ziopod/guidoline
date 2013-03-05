7137904e66cc46a6bbecd73f3ee7a93cd61fa447
bertrand keller <bertrand.keller@gmail.com>
2013-03-05 16:56:51 +0100

    improve app

--- app/js/main.js
+++ app/js/main.js
@@ -1,55 +1,19 @@
-var app = angular.module('myApp', ['ngGrid']);
-app.controller('MyCtrl', function($scope, $http) {
-    $scope.filterOptions = {
-        filterText: "",
-        useExternalFilter: true
-    };
-    $scope.pagingOptions = {
-        pageSizes: [250, 500, 1000],
-        pageSize: 250,
-        totalServerItems: 0,
-        currentPage: 1
-    };	
-    $scope.setPagingData = function(data, page, pageSize){	
-        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-        $scope.myData = pagedData;
-        $scope.pagingOptions.totalServerItems = data.length;
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
-    };
-    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
-        setTimeout(function () {
-            var data;
-            if (searchText) {
-                var ft = searchText.toLowerCase();
-                $http.get('json/inscrits2.json').success(function (largeLoad) {		
-                    data = largeLoad.filter(function(item) {
-                        return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
-                    });
-                    $scope.setPagingData(data,page,pageSize);
-                });            
-            } else {
-                $http.get('json/inscrits2.json').success(function (largeLoad) {
-                    $scope.setPagingData(largeLoad,page,pageSize);
-                });
-            }
-        }, 100);
-    };
-	
-    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-	
-    $scope.$watch('pagingOptions', function () {
-        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    }, true);
-    $scope.$watch('filterOptions', function () {
-        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    }, true);   
-	
-    $scope.gridOptions = {
-        data: 'myData',
-        enablePaging: true,
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions
-    };
-});
+function MyCtrl($scope) {
+		$.getJSON('json/inscrits2.json', function(data) {
+        $scope.$apply(function(){
+            $scope.myData = data;
+        });
+    });
+    
+		$scope.gridOptions = { 
+			data : 'myData',
+			enableCellSelection: true,
+			canSelectRows: false
+		};// $scope.myData is also acceptable but will not update properly. OK to use the object if you don't care about updating the data in the grid.
+		
+		$scope.save = function(membre){
+		  $scope.myData.push(angular.copy(membre));
+			$http.put('json/aaa.json', $scope.membre);
+			console.log($scope.myData);
+		};
+}
--- index.php
+++ index.php
@@ -3,12 +3,42 @@
 <head>
   <meta charset="utf-8">
   <title>My AngularJS App</title>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
   <link rel="stylesheet" href="app/css/app.css"/>
 	<link rel="stylesheet" href="app/css/ng-grid.css"/>
 </head>
 <body ng-controller="MyCtrl">
-
-	<div class="gridStyle" ng-grid="gridOptions"></div>
+	<div class="container">
+		<form name="membre" ng-submit="save(membre)">
+			<fieldset>
+			<legend>Ajouter un utilisateur</legend>
+		   <div class="form-item">
+				<label>Prénom:</label>
+			 <input ng-model="membre.prenom" type="text" required/><br />
+			</div>
+		  <div class="form-item"> 
+			<label>Nom:</label>
+			 <input ng-model="membre.nom" type="text" required/><br />
+			</div>
+			<div class="form-item">
+			 <label>Adresse:</label>
+			 <input ng-model="membre.adresse" type="text" required/><br />
+			</div>
+			<div class="form-item">
+			 <label>Profession:</label>
+			 <input ng-model="membre.profession" type="text" required/><br />
+			</div>
+			<div class="form-item">
+			 <label>Email:</label>
+			 <input ng-model="membre.email" type="email" required /><br />
+			</div>
+		   <button class="btn btn-primary">Save</button>
+			<fieldset>
+		</form>
+		<h2>Tableau des utilisateurs</h2>
+		<div class="gridStyle" ng-grid="gridOptions"></div>
+	
+	</div>
 
   <!-- In production use:
   <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js"></script>
@@ -16,6 +46,7 @@
   <script type="text/javascript" src="app/lib/jquery-1.8.2.min.js"></script>
 	<script src="app/lib/angular/angular.js"></script>
 	<script src="app/lib/ng-grid/ng-grid-1.9.0.debug.js"></script>
+	<script src="app/lib/ng-grid/ng-grid-flexible-height.js"></script>
   <script src="app/js/app.js"></script>
   <script src="app/js/services.js"></script>
   <script src="app/js/controllers.js"></script>
@@ -24,3 +55,5 @@
 	<script src="app/js/main.js"></script>
 </body>
 </html>
+
+
