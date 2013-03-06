fe9ad91ea2777f38929ba650c6b578b0f1a81464
ziopod <ziopod@gmail.com>
2013-03-06 16:45:15 +0100

    Nettoyage du bazar

--- app/js/test.js
+++
@@ -1,8 +0,0 @@
-function MyCtrl($scope) {
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
-    $scope.gridOptions = { data : 'myData' };// $scope.myData is also acceptable but will not update properly. OK to use the object if you don't care about updating the data in the grid.
-}
--- post.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * if post
- * vérifier le json
- * archiver le json précédent
- * enregistrer le nouveau
-*/
