f3afabd9a2840951a10b282f34c79d037750c268
ziopod <ziopod@gmail.com>
2013-03-15 01:55:24 +0100

    Ajout de la fiche profil

---
+++ application/classes/View/Users/Profil.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Fiche Membre
+* 
+* @package    Guidoline
+* @category   View model
+* @author     Ziopod | ziopod@gmail.com
+* @copyright  BY-SA 2013 Ziopod
+* @license    http://creativecommons.org/licenses/by-sa/3.0/deed.fr
+*/
+
+class View_Users_Profil extends View_Layout {
+
+}
