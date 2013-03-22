1195b1cfcc0ef4f05ba646009918c0af3bfe1635
bertrand keller <bertrand.keller@gmail.com>
2013-03-22 11:33:12 +0100

    update

--- application/classes/View/Members/Profil.php
+++ application/classes/View/Members/Profil.php
@@ -4,7 +4,7 @@
 * Fiche Membre
 * 
 * @package    Guidoline
-* @category   View model
+* @category   View Model
 * @author     Ziopod | ziopod@gmail.com
 * @copyright  BY-SA 2013 Ziopod
 * @license    http://creativecommons.org/licenses/by-sa/3.0/deed.fr
--- application/classes/View/Sandbox/Index.php
+++ application/classes/View/Sandbox/Index.php
@@ -4,7 +4,7 @@
 * Le modèle de vue `View/Sandbox/Index.php` fournis les propriétés et méthodes pour le template 'templates/sandbox/index.mustache'
 *
 * @package    Guidoline
-* @category   View model
+* @category   View Model
 * @author     Ziopod | ziopod@gmail.com
 * @copyright  BY-SA 2013 Ziopod
 * @license    http://creativecommons.org/licenses/by-sa/3.0/deed.fr
