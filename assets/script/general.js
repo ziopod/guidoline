02fcb1b3276e467fde44b2c9daad8f264513f82a
bertrand keller <bertrand.keller@gmail.com>
2013-06-06 19:01:15 +0200

    style login page

--- assets/script/general.js
+++ assets/script/general.js
@@ -3,11 +3,7 @@ $(function(){
 		$('.modale').nyroModal();
 		$('.tip').tooltipster({position: 'bottom'});
 		$('.tipleft').tooltipster({position: 'left'});
-		$("#js-main-menu").mmenu({
-		   configuration     : {
-		      slideDuration        : 20000
-		   }
-		});
+		$("#js-main-menu").mmenu({});
 		
 		var oTable = $('.datatable').dataTable( {
 			"sDom": "<'row'<'span8'l><'span8'f>r>t<'row'<'span8'i><'span8'p>>",
