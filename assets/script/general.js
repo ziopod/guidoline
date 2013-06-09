8a8a635e3eccf4c5fc97e1b1c43e5e8f02ab9d58
bertrand keller <bertrand.keller@gmail.com>
2013-06-09 09:12:36 +0200

    add js

--- assets/script/general.js
+++ assets/script/general.js
@@ -1,6 +1,6 @@
 
 $(function(){
-
+		document.body.className = document.body.className.replace("no-js","js");
 		$('.modale').nyroModal();
 		$('.tip').tooltipster({position: 'bottom'});
 		$('.tipleft').tooltipster({position: 'left'});
