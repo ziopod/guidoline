5b14cb91fde0bf235c474ccc719343bca0541814
bertrand keller <bertrand.keller@gmail.com>
2013-07-09 19:53:16 +0200

    add delay

--- assets/script/general.js
+++ assets/script/general.js
@@ -25,8 +25,9 @@ $(function(){
 		var oTableMembers = $('#table_members').dataTable( {
 			"sDom": "<'row'<'span8'l><'span8'f>r>t<'row'<'span8'i><'span8'p>>",
 			"sPaginationType": "bootstrap",
+			"sEcho": 0,
 			"iDisplayLength": 10,
-			"bStateSave": true,
+			//"bStateSave": true,
 			"aaSorting": [[ 2, "desc" ]], 
 			"aoColumns": [
 						null,
