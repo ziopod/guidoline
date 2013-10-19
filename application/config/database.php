6e0b4873bd8ec747fbde260900f2848bc38d1ff5
bertrand keller <bertrand.keller@gmail.com>
2013-10-19 10:54:47 +0200

    test

--- application/config/database.php
+++ application/config/database.php
@@ -20,10 +20,10 @@ return array
 			 *
 			 * Ports and sockets may be appended to the hostname.
 			 */
-			'hostname'   => $database_url['host'],
-			'database'   => substr($database_url['path'], 1),
-			'username'   => $database_url['user'],
-			'password'   => $database_url['pass'],
+			'hostname'   => 'localhost',
+			'database'   => 'guidoline',
+			'username'   => 'root',
+			'password'   => 'root',
 
 			'persistent' => FALSE,
 		),
