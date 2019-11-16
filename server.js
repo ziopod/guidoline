aff8382e9d84a63a290d35c5deb866d3579f4145
Ziopod <ziopod@gmail.com>
2019-11-16 04:53:12 +0100

    (ajout) surveiller php

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const browserSync = require("browser-sync");
-const sass = require('node-sass');
+const sass = require("node-sass");
 const { exec } = require("child_process");
 const proxy = process.argv[2];
 const port = process.argv[3];
 // Sass configuration
-const sass_source = 'assets/sass/';
-const sass_glob = '/**/*.scss';
-const sass_destination = 'assets/css/';
+const sass_source = "assets/sass/";
+const sass_glob = "**/*.scss";
+const sass_destination = "assets/css/";
+// Template configuration
+const template_source = "application/templates/";
+const template_glob = "**/*.mustache";
+// PHP classes configuration
+const php_source = "application/classes";
+const php_glob = "**/*.php";
 
 /**
  * Serveur local de développement
  *
  * Permettant le rechargement en direct et la compilation de Sass.
- * 
+ *
  * Installation :
  * ~~~
  * npm install
@@ -37,53 +43,71 @@ browserSync.init({
   open: false,
   watch: true,
 });
-  
+
+/* Watch & build Sass */
 browserSync
-  .create()
-  .watch(path.join(sass_source, sass_glob), (event, file) => {
+.create()
+.watch(path.join(sass_source, sass_glob), (event, file) => {
 
-    if (event === 'change') {
-      const filepath = path.dirname(file.replace(sass_source, ''));
-      const basename = path.basename(file, path.extname(file));
-      const file_destination = path.join(sass_destination, filepath);
-      
-      if ( ! fs.existsSync(file_destination))
-      {
-        fs.mkdir(file_destination, {recursive: true}, (err) => {
-          if (err) throw err;
-        });  
-      }
+  if (event === "change") {
+    const filepath = path.dirname(file.replace(sass_source, ""));
+    const basename = path.basename(file, path.extname(file));
+    const file_destination = path.join(sass_destination, filepath);
 
-      const destination_file = path.join(file_destination, basename) + '.css';
-
-      sass.render({
-        file: file,
-        outFile: destination_file,
-        sourceMap: true,
-      }, (error, result) => {
-        if( ! error){
-
-          fs.writeFile(destination_file, result.css, (err) => {
-            if ( ! err) {
-              console.log("Compilation de " + file);
-              browserSync.notify("Injection de " + file);
-              throw err;
-            }
-            else
-            {
-              console.log("Erreur lors de la compilation : " + err);
-              browserSync.notify("Erreur lors d'inejection' : " + err);
-            }
-          });
-
-          browserSync.notify(result);
-
-        } else {
-          browserSync.notify(error);
-        }
-      })
-
-      browserSync.reload('*.css');
-      browserSync.notify("Modification de fichiers " + file);
+    if ( ! fs.existsSync(file_destination))
+    {
+      fs.mkdir(file_destination, {recursive: true}, (err) => {
+        if (err) throw err;
+      });
     }
-  });
+
+    const destination_file = path.join(file_destination, basename) + ".css";
+
+    sass.render({
+      file: file,
+      outFile: destination_file,
+      sourceMap: true,
+    }, (error, result) => {
+      if( ! error){
+
+        fs.writeFileSync(destination_file, result.css, (err) => {
+          if ( ! err) {
+            console.log("Compilation de " + file);
+            browserSync.notify("Injection de " + file);
+            throw err;
+          }
+          else
+          {
+            console.log("Erreur lors de la compilation : " + err);
+            browserSync.notify("Erreur lors de l'injection : " + err);
+          }
+        });
+
+        console.log(result);
+
+      } else {
+        console.log(error);
+      }
+    })
+
+    browserSync.notify("Modification du fichier " + file);
+    browserSync.reload("*.css");
+  }
+});
+
+/* Watch Template */
+browserSync.watch(path.join(template_source, template_glob), (event, file) => {
+  if (event === "change") {
+    // Just reload
+    browserSync.notify("Modification de " + file);
+    browserSync.reload();
+  }
+});
+
+browserSync.watch(path.join(php_source, php_glob), (event, file) => {
+  if (event === "change") {
+    // Just reload
+    browserSync.notify("Modification de " + file);
+    browserSync.reload();
+  }
+});
