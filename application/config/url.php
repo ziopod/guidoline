4aacc1d9a4e93bdcdc7f9ab3e9c75165e9a8156d
Ziopod <ziopod@gmail.com>
2019-11-28 21:50:11 +0100

    (correction) usage de "trusted_hosts"  via le fichier Dotenv

--- application/config/url.php
+++ application/config/url.php
@@ -1,18 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 return array(
-
-	'trusted_hosts' => array(
-		// Set up your hostnames here
-		//
-		// Example:
-		//
-		//        'example\.org',
-		//        '.*\.example\.org',
-		//
-		// Do not forget to escape your dots (.) as these are regex patterns.
-		// These patterns should always fully match,
-		// as they are prepended with `^` and appended with `$`
-	),
-
+  /**
+   * Setup Trusted Host in dotenv
+   *
+   * ~~~
+   * TRUSTED_HOSTS='example.org,.*.example.org'
+   * ~~~
+   */
+  'trusted_hosts' => explode(',',getenv('TRUSTED_HOSTS')),
 );
