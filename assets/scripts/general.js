4b495f9da8ec3efe5984d9b6fb923e99e6457b6f
Ziopod <ziopod@gmail.com>
2019-11-28 22:35:43 +0100

    (nettoyage) assets inutilisés

--- assets/scripts/general.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-		document.body.className = document.body.className.replace("no-js","js");
-		$('.modale').nyroModal();
-		$('.tip').tooltipster({position: 'bottom'});
-		$('.tipleft').tooltipster({position: 'left'});
-		$("#js-main-menu").mmenu();
-});
