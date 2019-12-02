978974a6af495e2d546cb54a05e24868c6fc1a81
Ziopod <ziopod@gmail.com>
2019-12-02 20:59:40 +0100

    (amélioration) nom et durée de session

--- application/config/session.php
+++ application/config/session.php
@@ -4,11 +4,5 @@ return array(
     'native' => array(
       'name' => getEnv('SESSION_NAME'),
       'lifetime' => 86400,
-    ),
-    'cookie' => array(
-      'name' => getEnv('COOKIE_NAME'),
-      'encrypted' => TRUE,
-      'salt' => getEnv('COOKIE_SALT'),
-      'lifetime' => 86400,
     )
 );
