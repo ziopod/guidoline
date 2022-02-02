0b4b7f24d1185689f2d340707aa04335bd1cd56f
Ziopod <ziopod@gmail.com>
2022-02-02 15:05:06 +0100

    Gestion des clefs en variable d'environnement

--- application/config/mailjet.php
+++ application/config/mailjet.php
@@ -6,10 +6,7 @@
 return array(
   'useragent' => 'Guidoline Members',
   'url' => 'https://api.mailjet.com/v3/REST/',
-  'api_key' => '4f769c42da5cb9170944b5faefcf8cbe',
-  'api_secret' => '375f0694449f7fe6e2f5cf8f8c834368',
-  'list' => array(
-    'members' => '9632',
-    'external' => '9630'
-  )
+  'api_key' => getenv('MAILJET_KEY'),
+  'api_secret' => getenv('MAILJET_SECRET'),
+  'list_id' => getenv('MAILJET_LIST_ID')
 );
